bca618f7a8ae50d3f1ec52a618774a5dd729d1ae
Justine L. Llamera <jllamera06@gmail.com>
2025-09-14 00:56:33 +0800

    v2

--- src/app/components/DrawerNav.tsx
+++ src/app/components/DrawerNav.tsx
@@ -24,18 +24,25 @@ export default function DrawernNavigation ({setOpenDrawnerNav} : DrawerTypes) {
         <>
 
         {/* Base 80px, mas 180px */}
-        <div className="absolute h-full w-[80px] bg-amber-800">
-            <div className="flex flex-col w-full h-full items-center">
-                {navItems.map((item, index) => (
-                    <div
-                    key={index}
-                    className=""
-                    >
-                        <p>
-                            {item.name}
-                        </p>
+        <div className="h-full w-[280px] bg-slate-950 rounded-e-xl">
+            <div className="flex flex-col w-full h-full">
+                <div className="flex gap-2 p-4 flex-col">
+
+                    <div className="mb-6">
+                        <h1 className="text-md font-bold text-white">Student Performance</h1>
                     </div>
-                ))}
+
+                    {navItems.map((item, index) => (
+                        <div
+                        key={index}
+                        className="cursor-pointer pl-1.5 p-0.5 rounded-sm text-white hover:bg-[rgba(241,245,249,0.18)] transition-all duration-300"
+                        >
+                            <p className="text-md font-bold">
+                                {item.name}
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,47 @@
 import { useState } from "react";
 import DrawernNavigation from "./components/DrawerNav";
 import { Button } from "@/components/ui/button"
- 
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
 
 export default function Home() {
   const [openDrawerNav, setOpenDrawerNav] = useState<boolean>(false);
 
   return (
-    <div className="">
-      <main className="">
+    <div className="w-screen h-screen">
+      <main className="flex h-full w-full">
 
         <DrawernNavigation
           setOpenDrawnerNav={setOpenDrawerNav}
         />
 
+        <div className="flex flex-col w-full p-6">
 
-        {/* <Button> Oh girl its youu</Button> */}
+          <div className="">
+            <Card>
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+                <CardAction>Card Action</CardAction>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
 
-        <p>Dashboard Projext</p>
+        </div>
+
       </main>
     </div>
   );
