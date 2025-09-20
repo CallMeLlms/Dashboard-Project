c61a345d7bd95f73e4472caaa982356820c2561f
Justine L. Llamera <jllamera06@gmail.com>
2025-09-20 15:21:00 +0800

    lov

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DrawernNavigation from "@/components/custom/DrawerNav";
 import { Button } from "@/components/ui/button"
 import CustomCardComp from "@/components/custom/CustomCard";
-import ChartAreaDefault from "@/components/custom/CardChart"; 
+import ChartAreaDefault from "@/components/custom/CardChart";
 import { ChartBarMixed } from "@/components/custom/BarChart";
-
-import {Papa} from "papaparse"
+import Papa from "papaparse"
 
 export default function Home() {
   const [openDrawerNav, setOpenDrawerNav] = useState<boolean>(false);
@@ -29,11 +28,11 @@ export default function Home() {
 
       {/* Main Content */}
       <div className="flex flex-col flex-1">
-        
+
         {/* Component */}
         <header className="h-16 border-b flex items-center justify-between px-6 rounded-2xl bg-slate-950 mt-2 mr-2">
           <div className="text-white">Search / Breadcrumb</div>
-          
+
           {/* Drop Down filter */}
           <div className="text-white">User Menu</div>
         </header>
@@ -43,19 +42,20 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {/* Example widgets */}
             {/* Widgest compomnents */}
-            
+
             {[...Array(repeat)].map((_, i) => (
-              <CustomCardComp key={i}/>
+              <CustomCardComp key={i} />
             ))}
 
+
             <div className="col-span-1">
-              <ChartBarMixed/>
+              <ChartBarMixed />
             </div>
 
             <div className="col-span-2">
-              <ChartAreaDefault/>
+              <ChartAreaDefault />
             </div>
-            
+
             {/* <div className="rounded-2xl border p-4 shadow-sm">Card 3</div> */}
           </div>
 
