e54aab004b9363a149a90535b2acc20e8e5e2635
Justine L. Llamera <jllamera06@gmail.com>
2025-10-05 18:13:09 +0800

    arghh tangina

--- src/components/custom/DrawerNav.tsx
+++ src/components/custom/DrawerNav.tsx
@@ -18,28 +18,16 @@ export default function DrawerNavigation({ isOpen, setIsOpen }: DrawerTypes) {
 
     return (
         <div 
-            className={`
-                relative h-full bg-slate-950 rounded-2xl transition-all duration-300 ease-in-out
-                ${isOpen ? 'left-[0]' : 'left-[-160]'}
-            `}
+            className={`relative h-full bg-slate-950 rounded-2xl transition-all duration-300 ease-in-out`}
         >
             {/* Toggle Button */}
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                className="absolute -right-3 top-8 bg-slate-950 border-2 border-slate-800 rounded-full p-1 hover:bg-slate-800 transition-colors z-10"
-            >
-                {isOpen ? (
-                    <ChevronLeft className="w-4 h-4 text-white" />
-                ) : (
-                    <ChevronRight className="w-4 h-4 text-white" />
-                )}
-            </button>
+
 
             <div className="flex flex-col w-full h-full">
                 <div className="flex gap-2 p-4 flex-col">
                     {/* Header */}
-                    <div className={`mb-6 overflow-hidden transition-all duration-300 ${isOpen ? 'opacity-100' : 'opacity-0'}`}>
-                        <h1 className={`text-md font-bold text-white whitespace-nowrap ${!isOpen && 'invisible'}`}>
+                    <div className={`mb-6 overflow-hidden transition-all duration-300`}>
+                        <h1 className={`text-md font-bold text-white whitespace-nowrap`}>
                             Student Dashboard
                         </h1>
                     </div>
