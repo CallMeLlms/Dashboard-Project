ae6e580879a4202dda2ec89bd85e1a26e319fdd2
Justine L. Llamera <jllamera06@gmail.com>
2025-12-22 21:52:57 +0800

    CSV Context

--- src/components/custom/CustomCard.tsx
+++ src/components/custom/CustomCard.tsx
@@ -1,84 +1,64 @@
 "use client"
 import {
-    Card,
-    CardHeader,
-    CardFooter,
-    CardTitle,
-    CardAction,
-    CardDescription,
-    CardContent,
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
 } from "@/components/ui/card";
 import { useEffect, useMemo, useState } from "react";
-import { ParsedTitleName } from "@/app/utils/nameParse";
-import { SkeletonSmall, SkeletonMedium, SkeletonLarge } from "../custom/skeletonLoader";
+import { SkeletonSmall, SkeletonMedium, SkeletonLarge } from "./SkeletonLoader";
 
 
-type CustomCardCompTypes = {
+export default function CustomCardComp({ }) {
 
-}
+  const [studData, setStudData] = useState<any>([]);
 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch('api/parse-csv')
+        const data = await response.json();
+        setStudData(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getData();
+  }, [])
 
-export default function CustomCardComp({}) {
+  //   Use memo for this 1
+  const handleData = useMemo(() => {
+    const totalHoursStudied = studData.reduce((total: number, item: any) => total + item.Hours_Studied, 0)
+    return totalHoursStudied
+  }, [studData])
 
-      const [studData, setStudData] = useState<any>([]);
-      const [titleParse, setTitleParsed] = useState<any>([]);
+  // const handleTitleData =  async () => {
+  //     const title = Object.keys(studData[0])
+  //     return title
+  // }
 
-      useEffect(() => {
-        const getData = async() => {
-          try {
-            const response = await fetch('/api/parseCsv')
-              const data = await response.json();
-              setStudData(data)     
-          } catch(err) {
-            console.log(err)
-          }
-        }
-        getData();
-      }, [])
+  // console.log(handleTitleData())
 
-      useEffect(() => {
-        const titleParsed = async () => {
-          try{
-            const response = await ParsedTitleName();
-            if(!response) {
-              console.log("Encountered error")
-            }
-            setTitleParsed(response)
-          } catch (err) {
-            console.log(err)
+  return (
+    <>
+      <Card className="border-2 border-[rgb(200,200,200)]">
+        <CardHeader>
+          <CardTitle
+            className="text-2xl font-bold"
+          >{studData === 'Hours_Studied' ? "Hours Studied" : <SkeletonMedium />}</CardTitle>
 
-          }
-        }
-        titleParsed();
-      }, [])
+          <CardDescription className="text-md font-medium">Hours Dedicated</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="text-4xl font-bold">{handleData}</p>
+        </CardContent>
+        <CardFooter>
 
-    //   Use memo for this 1
-      const handleData = useMemo(() => {
-        const totalHoursStudied = studData.reduce((total: number, item: any) => total + item.Hours_Studied, 0)
-        return totalHoursStudied
-      }, [studData])
-
-    return (
-        <>
-            <Card className="border-2 border-[rgb(200,200,200)]">
-                <CardHeader>
-                    <CardTitle
-                    className="text-2xl font-bold"
-                    >{titleParse[0] === 'Hours_Studied' ? "Hours Studied" : <SkeletonMedium/> }</CardTitle>
-
-                    <CardTitle
-                    className="text-2xl font-bold"
-                    >{titleParse[1]  ? `${titleParse[10]}` : <SkeletonMedium/> }</CardTitle>
-                    
-                    <CardDescription className="text-md font-medium">Hours Dedicated</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p className="text-4xl font-bold">{handleData}</p>
-                </CardContent>
-                <CardFooter>
-                    
-                </CardFooter>
-            </Card>
-        </>
-    )
+        </CardFooter>
+      </Card>
+    </>
+  )
 }
